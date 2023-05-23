440e3992f230794b84b0767c01598af7e869b646
Sarawanan <sarawananm@icloud.com>
2023-05-23 10:22:12 +0800

    Create Employee.java

---
+++ src/main/java/com/example/employee/Employee.java
@@ -0,0 +1,24 @@
+package com.example.employee;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Float salary;
+    @ManyToOne
+    private Department department;
+}
