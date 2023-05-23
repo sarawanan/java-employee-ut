5bd27d6d90b0e1e506ef2dc3af4015b82e028ba0
Sarawanan <sarawananm@icloud.com>
2023-05-23 10:22:33 +0800

    Create DepartmentRepo.java

---
+++ src/main/java/com/example/employee/DepartmentRepo.java
@@ -0,0 +1,9 @@
+package com.example.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department, Long> {
+    Department findDepartmentByName(String name);
+}
